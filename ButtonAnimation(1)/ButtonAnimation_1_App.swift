bbd28561b33a2d4332e359efb85402c50a1d32a2
Berk Aksu <berkaksu79@gmail.com>
2023-12-18 16:00:59 +0300

    Initial Commit

---
+++ ButtonAnimation(1)/ButtonAnimation_1_App.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonAnimation_1_App.swift
+//  ButtonAnimation(1)
+//
+//  Created by Berk Aksu on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ButtonAnimation_1_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
